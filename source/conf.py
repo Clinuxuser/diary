017f3e082047f29c6d06e446e7830529d8d99755
dongzl <1850980494@qq.com>
2023-03-27 00:22:23 +0800

    文件托管服务器不支持markdown

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ release = 'v1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables']
+#extensions = ['recommonmark','sphinx_markdown_tables']
 
 templates_path = ['_templates']
 exclude_patterns = []
